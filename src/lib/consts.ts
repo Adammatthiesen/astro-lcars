0558953c74e0dc411b186f4157875ab730bff551
Adam Matthiesen <amatthiesen@outlook.com>
2026-03-19 08:03:27 -0700

    Add LICENSE constant and update footer copyright information in LCARSFrame.astro

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -5,6 +5,26 @@ import type { NavData } from "./types";
 export const SOURCE_URL = "https://github.com/withstudiocms/astro-lcars";
 
 /**
+ * License information for the LCARS-themed website, indicating that the content is MIT Licensed and providing attribution to the company "Astro LCARS - withStudioCMS" as the creator and maintainer of the project, ensuring that users are aware of the licensing terms and the source of the content they are using or contributing to.
+ * 
+ * @example
+ * ```json
+ * {
+ *   "preamble": "MIT Licensed",
+ *   "company": "Astro LCARS - withStudioCMS"
+ * }
+ * ```
+ * 
+ * Will render as:
+ * 
+ * MIT Licensed {currentYear} Astro LCARS - withStudioCMS.
+ */
+export const LICENSE = {
+    preamble: "MIT Licensed",
+    company: "Astro LCARS - withStudioCMS",
+}
+
+/**
  * Navigation data for the LCARS-themed website, defined as an array of NavData objects that represent the different navigation items, including their text labels, hyperlink references, unique keys for identification, and optional colors from the LCARS color palette to provide visual distinction and thematic consistency throughout the design.
  */
 export const NAV: NavData[] = [
